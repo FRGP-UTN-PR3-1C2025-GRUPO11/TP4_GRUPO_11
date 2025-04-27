595323bdc12d606d131e622898c2b6443cf52aa9
Fede-Giithub <federico.pierdominici@gmail.com>
2025-04-27 20:03:47 -0300

    ej3_CargadeBasedeDatos

--- TP4_Grupo_11/Ejercicio03.aspx.cs
+++ TP4_Grupo_11/Ejercicio03.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Drawing;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,8 +11,25 @@ namespace TP4_Grupo_11
 {
     public partial class Ejercicio03 : System.Web.UI.Page
     {
+        private const string cadenaConexion = "Data Source=DESKTOP-6LDIHKB\\SQLEXPRESS;Initial Catalog = Libreria; Integrated Security = True;TrustServerCertificate=True";
         protected void Page_Load(object sender, EventArgs e)
         {
+
+            if (!IsPostBack)
+            {
+                SqlConnection connection = new SqlConnection(cadenaConexion);
+                connection.Open();
+
+                SqlCommand sqlCommand = new SqlCommand("SELECT * FROM Temas", connection);
+                SqlDataReader sqlDatareader = sqlCommand.ExecuteReader();
+
+                ddlTemas.DataSource = sqlDatareader;
+                ddlTemas.DataTextField = "Tema";
+               ddlTemas.DataValueField = "IdTema";
+               ddlTemas.DataBind();
+
+                connection.Close();
+            }
 
         }
     }
